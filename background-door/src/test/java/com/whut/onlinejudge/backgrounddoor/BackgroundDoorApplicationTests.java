04860772961cdcba7fb7f26e41db051e2589a976
liuqiao <349953@whut.edu.cn>
2024-11-29 13:37:40 +0800

    project initialization

---
+++ background-door/src/test/java/com/whut/onlinejudge/backgrounddoor/BackgroundDoorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whut.onlinejudge.backgrounddoor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackgroundDoorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
