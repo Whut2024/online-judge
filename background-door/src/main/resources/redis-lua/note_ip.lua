b57a38ea487ab394242878c20262d8431ac9cdcc
liuqiao <349953@whut.edu.cn>
2024-12-29 10:41:57 +0800

    modify redis script bug - 2

--- background-door/src/main/resources/redis-lua/note_ip.lua
+++ background-door/src/main/resources/redis-lua/note_ip.lua
@@ -1,6 +1,1 @@
-if not (redis.call('get', KEYS[1]) == nil) then
-    redis.call('expire', KEYS[1], ARGV[1])
-    return 0
-end
-redis.call('setex', KEYS[1], ARGV[1], 1)
-return 1
+if not (redis.call('get', KEYS[1]) == '1') then redis.call('setex', KEYS[1], ARGV[1], 1) return 1 end redis.call('expire', KEYS[1], ARGV[1]) return 0
