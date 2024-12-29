473ae7575abee09f792f4856ca2da87bada41b35
liuqiao <349953@whut.edu.cn>
2024-12-29 10:08:29 +0800

    modify redis script bug

--- background-door/src/main/java/com/whut/onlinejudge/backgrounddoor/aop/OtherIpNoticeAdvisor.java
+++ background-door/src/main/java/com/whut/onlinejudge/backgrounddoor/aop/OtherIpNoticeAdvisor.java
@@ -47,7 +47,7 @@ public class OtherIpNoticeAdvisor {
 
     static {
         NOTE_IP = new DefaultRedisScript<>();
-        NOTE_IP.setScriptText("if not redis.call('get', KEYS[1]) == nil then redis.call('expire', KEYS[1], ARGV[1]) return 0 end redis.call('setex', KEYS[1], ARGV[1], 1) return 1");
+        NOTE_IP.setScriptText("if not (redis.call('get', KEYS[1]) == nil) then redis.call('expire', KEYS[1], ARGV[1]) return 0 end redis.call('setex', KEYS[1], ARGV[1], 1) return 1");
         NOTE_IP.setResultType(Long.class);
         /**
          * if not redis.call('get', KEYS[1]) == nil then
