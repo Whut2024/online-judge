6e601d1bf2fa5fc5a1b0d9155f38540f3f48efc9
liuqiao <349953@whut.edu.cn>
2025-03-23 22:45:12 +0800

    delete some dubbo files

--- background-door/src/main/java/com/whut/onlinejudge/backgrounddoor/BackgroundDoorApplication.java
+++ background-door/src/main/java/com/whut/onlinejudge/backgrounddoor/BackgroundDoorApplication.java
@@ -1,13 +1,11 @@
 package com.whut.onlinejudge.backgrounddoor;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.whut.onlinejudge.backgrounddoor.mapper")
-@EnableDubbo
 public class BackgroundDoorApplication {
 
     public static void main(String[] args) {
--- core/src/main/java/com/whut/onlinejudge/core/CoreApplication.java
+++ core/src/main/java/com/whut/onlinejudge/core/CoreApplication.java
@@ -1,7 +1,6 @@
 package com.whut.onlinejudge.core;
 
 import com.whut.onlinejudge.core.config.CodeRunnerConfig;
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.whut.onlinejudge.core.mapper")
-@EnableDubbo(scanBasePackages = {"com.whut.onlinejudge.core.service.impl"})
 @EnableConfigurationProperties(CodeRunnerConfig.class)
 @EnableScheduling
 public class CoreApplication {
