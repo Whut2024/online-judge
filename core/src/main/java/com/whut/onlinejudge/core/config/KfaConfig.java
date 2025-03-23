1951f06c654a5f492ecc9a29970d74a4144541e2
liuqiao <349953@whut.edu.cn>
2025-03-23 19:59:45 +0800

    import kafka model

---
+++ core/src/main/java/com/whut/onlinejudge/core/config/KfaConfig.java
@@ -0,0 +1,12 @@
+package com.whut.onlinejudge.core.config;
+
+/**
+ * @author liuqiao
+ * @since 2025-03-23
+ */
+public class KfaConfig {
+    
+    public static final String TOPIC = "oj-submission";
+    
+    public static final String GROUP_ID = "oj-core";
+}
