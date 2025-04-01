aa74d2ef47bf9634382ded44da01a47fdd4af9a8
liuqiao <349953@whut.edu.cn>
2025-04-01 10:39:49 +0800

    add interceptor

--- initialize/router.go
+++ initialize/router.go
@@ -3,14 +3,22 @@ package initialize
 import (
 	"github.com/gin-gonic/gin"
 	"go-oj/global"
+	"go-oj/interceptor"
 	"go-oj/router"
 )
 
 func InitRouter(engine *gin.Engine) {
+
+	// 添加全局 interceptor
+	engine.Use(interceptor.GinLogger, interceptor.GinRecovery(false))
+
 	// 三种权限校验的路由组
 	publicGroup := engine.Group(global.Config.ContextPath)
 	privateGroup := engine.Group(global.Config.ContextPath)
 	adminGroup := engine.Group(global.Config.ContextPath)
+
+	// 添加 interceptor
+	privateGroup.Use(interceptor.CheckToken)
 
 	// 不同模块接口路由初始化方法的总集成对象
 	routerGroup := router.RouterGroupObj
---
+++ interceptor/auth.go
@@ -0,0 +1,60 @@
+package interceptor
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/goccy/go-json"
+	"go-oj/constant"
+	"go-oj/entity/response"
+	"go-oj/global"
+	"strconv"
+)
+
+func CheckToken(c *gin.Context) {
+	// 获取 token
+	token := c.GetHeader("token")
+	if token == "" {
+		response.FailWithMessage("token 错误", c)
+		c.Abort()
+		return
+	}
+
+	// 获取存储在 redis 中 user 对象
+	key := constant.USER_CACHE + token
+	userJsonStr, e := global.Redis.Get(key).Result()
+	if e != nil {
+		response.FailWithMessage("请稍后再试", c)
+		c.Abort()
+		return
+	}
+	if len(userJsonStr) == 0 {
+		response.FailWithMessage("未登录", c)
+		c.Abort()
+		return
+	}
+
+	user := &response.UserVo{}
+	err := json.Unmarshal([]byte(userJsonStr), user)
+	if err != nil {
+		response.FailWithMessage("请稍后再试", c)
+		c.Abort()
+		return
+	}
+
+	// 获取存储在 redis 中 user 版本信息
+	key = constant.USER_LOGIN_VERSION_KEY + strconv.FormatInt(user.Id, 10)
+	version, e := global.Redis.Get(key).Int64()
+	if e != nil {
+		response.FailWithMessage("请稍后再试", c)
+		c.Abort()
+		return
+	}
+
+	if version != user.Version {
+		response.FailWithMessage("未登录", c)
+		c.Abort()
+		return
+	}
+	c.Abort()
+	c.Set(constant.USER_CACHE, user)
+	c.Next()
+}
